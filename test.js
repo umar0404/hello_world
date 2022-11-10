6581accd25596c824a63e522c757763c83b94b15
umar0404 <azimovarjon@gmail.com>
2022-11-10 17:35:46 +0500

    test

--- test.js
+++ test.js
@@ -3,31 +3,28 @@ import cors from "cors";
 import mongoose from "mongoose";
 import userSchema from "./models/userSchema.js";
 
-const dataConnect = () => {
-  mongoose
-    .connect(
-      "mongodb+srv://umar:040401@cluster0.1cmquul.mongodb.net/?retryWrites=true&w=majority"
-    )
-    .then(() => console.log("Successfully logged in"));
-};
-dataConnect();
-const PORT = process.env.PORT || 8083
-
+mongoose
+  .connect(
+    "mongodb+srv://umar:040401@cluster0.1cmquul.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Successfully logged in"));
+const PORT = process.env.PORT || 8083;
 
 const app = express();
 
 app.use(json());
 app.use(cors());
 
-app.get('/getAll', async (req, res)=>{
-    const data = await userSchema.find()
-    res.json(data)
-})
+app.get("/getAll", async (req, res) => {
+  const data = await userSchema.find();
+  res.json(data);
+});
 
 app.post("/post", async (req, res) => {
   try {
     const { userName, title } = req.body;
-    if(!userName || !title) return res.status(400).json({ message: "userName or title do not find" });
+    if (!userName || !title)
+      return res.status(400).json({ message: "userName or title do not find" });
     const post = new userSchema({ userName, title });
     await post.save();
     res.status(200).json({ message: "OK" });
@@ -35,10 +32,6 @@ app.post("/post", async (req, res) => {
     res.status(400).json({ message: "Error /post" });
   }
 });
-
-app.get('/test', (res, req)=> {
-    res.send('<h1>Server Working</h1>')
-})
 
 
 app.listen(PORT, () => console.log("Server yurdi " + PORT));
